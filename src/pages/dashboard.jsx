850e531400a705696aec57956ba1a6aa4231822e
JasYi <44511533+JasYi@users.noreply.github.com>
2024-09-14 17:21:32 -0400

    some dashboard work

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Page, LegacyCard, DataTable } from "@shopify/polaris";
+import { useSearchParams } from "react-router-dom";
+
+const Dashboard = () => {
+  const [searchParams] = useSearchParams();
+
+  const formID = searchParams.get("id");
+
+  const rows = [
+    ["Emerald Silk Gown", "$875.00", 124689, 140, "$122,500.00"],
+    ["Mauve Cashmere Scarf", "$230.00", 124533, 83, "$19,090.00"],
+    [
+      "Navy Merino Wool Blazer with khaki chinos and yellow belt",
+      "$445.00",
+      124518,
+      32,
+      "$14,240.00",
+    ],
+  ];
+
+  return (
+    <div>
+      <h1>Data Dashboard</h1>
+      <Page title="Sales by product">
+        <LegacyCard>
+          <DataTable
+            columnContentTypes={[
+              "text",
+              "numeric",
+              "numeric",
+              "numeric",
+              "numeric",
+            ]}
+            headings={[
+              "Product",
+              "Price",
+              "SKU Number",
+              "Net quantity",
+              "Net sales",
+            ]}
+            rows={rows}
+            totals={["", "", "", 255, "$155,830.00"]}
+          />
+        </LegacyCard>
+      </Page>
+    </div>
+  );
+};
+
+export default Dashboard;
