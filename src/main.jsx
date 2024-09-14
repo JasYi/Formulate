d1359b0bb521e1e02e4d2801b3daaa261d167b23
Andrew Ma <andrewmamusic@gmail.com>
2024-09-14 05:50:58 -0400

    button test

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import '@shopify/polaris/build/esm/styles.css';
+
+
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
