781c201401066e37163eed179759881ab8ad6a8d
JasYi <44511533+JasYi@users.noreply.github.com>
2024-09-15 02:45:50 -0400

    created seperate landing page

--- src/pages/submission.jsx
+++ src/pages/submission.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./submission.css";
 
 const Submission = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/"); // Navigate to a new page
+    }, 3000);
+
+    // Cleanup the timer when the component is unmounted
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
-    <div>
-      <h1>Thank you for your response!</h1>
+    <div className="container">
+      <h1 className="text main-txt">Thank you for your response!</h1>
+      <h2 className="text second-txt">
+        This page will redirect in 3 seconds...
+      </h2>
     </div>
   );
 };
